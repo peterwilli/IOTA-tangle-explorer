a3120246730ef8102b048387bdfa99daa73ddbb1
Peter <peter@codebuffet.co>
2017-07-21 00:14:23 +0200

    increase depth

--- tangle-explorer-web/src/components/TangleGraph.vue
+++ tangle-explorer-web/src/components/TangleGraph.vue
@@ -1,6 +1,11 @@
-j <template lang="html">
-  <div ref="graph">
-
+<template lang="html">
+  <div class="graph-container">
+    <div class="toolbar">
+      <div class="button" @click="increaseDepth()">
+        Increase Depth
+      </div>
+    </div>
+    <div class="graph" ref="graph"></div>
   </div>
 </template>
 
@@ -14,40 +19,44 @@ export default {
   props: ['txs', 'viewingHash'],
   methods: {
     processLabel(lbl) {
-      if(lbl === this.viewingHash) {
+      if (lbl === this.viewingHash) {
         return `*${lbl.substring(0, 5) + "..."}*`
-      }
-      else {
+      } else {
         return lbl.substring(0, 5) + "..."
       }
     },
     getNodeColor(hash) {
-      if(hash === this.viewingHash) {
+      if (hash === this.viewingHash) {
         return '#48aacf'
-      }
-      else {
+      } else {
         return '#ddb013'
       }
     },
     txsToNodes(txs) {
       var arr = []
-      for(var tx of txs) {
+      for (var tx of txs) {
         arr.push({
-          font: { multi: "md" },
+          font: {
+            multi: "md"
+          },
           id: tx.hash,
           color: this.getNodeColor(tx.hash),
           label: this.processLabel(tx.hash)
         })
 
         arr.push({
-          font: { multi: "md" },
+          font: {
+            multi: "md"
+          },
           id: tx.branchTransaction,
           color: this.getNodeColor(tx.branchTransaction),
           label: this.processLabel(tx.branchTransaction)
         })
 
         arr.push({
-          font: { multi: "md" },
+          font: {
+            multi: "md"
+          },
           id: tx.trunkTransaction,
           color: this.getNodeColor(tx.trunkTransaction),
           label: this.processLabel(tx.trunkTransaction)
@@ -61,7 +70,7 @@ export default {
     },
     txsToEdges(txs) {
       var arr = []
-      for(var tx of txs) {
+      for (var tx of txs) {
         arr.push({
           from: tx.hash,
           to: tx.branchTransaction
@@ -76,13 +85,23 @@ export default {
     },
     increaseDepth() {
       var arr = []
-      for(var tx of this.txsToRender) {
+      for (var tx of this.txsToRender) {
         arr.push(tx.branchTransaction)
         arr.push(tx.trunkTransaction)
       }
       var _this = this
+      arr = _.uniq(arr)
+      arr = _.filter(arr, (hash) => {
+        for(var tx of _this.txsToRender) {
+          if(tx.hash === hash) {
+            return false
+          }
+        }
+        return true
+      })
+
       iotaNode.iota.api.getTransactionsObjects(arr, function(e, r) {
-        for(var tx of r) {
+        for (var tx of r) {
           _this.txsToRender.push(tx)
         }
         _this.txsToRender = _.uniqBy(_this.txsToRender, (tx) => {
@@ -93,8 +112,8 @@ export default {
     },
     destroyNetwork() {
       if (this.network !== null) {
-          this.network.destroy()
-          this.network = null
+        this.network.destroy()
+        this.network = null
       }
     },
     update() {
@@ -110,25 +129,42 @@ export default {
       };
 
       var options = {
-          nodes: {
-              shape: 'dot',
-              size: 30,
-              font: {
-                  size: 20,
-                  color: '#ffffff'
-              },
-              borderWidth: 2
+        nodes: {
+          shape: 'dot',
+          size: 30,
+          font: {
+            size: 20,
+            color: '#ffffff'
           },
-          edges: {
-              width: 2
-          }
+          borderWidth: 2
+        },
+        edges: {
+          width: 2
+        }
       };
 
       this.network = new vis.Network(container, data, options)
+      var _this = this
+      this.network.on("click", function(params) {
+        if(this.getNodeAt(params.pointer.DOM) === undefined) {
+          return
+        }
+        _this.$router.push({
+          name: 'Transaction',
+          params: {
+            hash: this.getNodeAt(params.pointer.DOM)
+          }
+        })
+      })
     }
   },
   beforeDestroy() {
     this.destroyNetwork()
+  },
+  watch: {
+    txs() {
+      this.update()
+    }
   },
   data() {
     return {
@@ -145,7 +181,28 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-  div {
-    background: #222222
-  }
+  .graph-container
+    position relative
+
+  .toolbar
+    left 0
+    right 0
+    position absolute
+    background rgba(255, 255, 255, 0.2)
+    border-bottom 1px solid #333
+    z-index: 1
+    .button
+      color #fff
+      cursor pointer
+      padding 10px
+      float left
+      &:active
+        background rgba(0, 0, 0, 0.1)!important
+
+      &:hover
+        background rgba(255, 255, 255, 0.2)
+
+  .graph
+    background #222222
+    height 250px
 </style>
